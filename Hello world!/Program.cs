89c37e6b46c37b484fc894926d2812bad9bd8c98
Opilane TTHK <opilane@tthk.ee>
2021-11-09 09:50:56 +0200

    add first name

---
+++ Hello world!/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_world_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Hello, " + firstname + "!");
+
+            Console.Read();
+        }
+    }
+}
